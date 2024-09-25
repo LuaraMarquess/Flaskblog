39e4bb9298923d6d7fd4056c20930426cf02788f
LuaraNabor <luaranaboraci@gmail.com>
2024-09-25 11:52:42 -0300

    continuacao:2024.09.25

--- index.py
+++ index.py
@@ -1,25 +1,48 @@
-from flask import Flask   #from flask import Flask
+# Importa a classe Flask do módulo flask
+from flask import Flask, render_template
+
+# Cria uma instância da aplicação Flask
+app = Flask(__name__)
+
+########################
+# Tratamento das rotas #
+########################
 
 
-app = Flask(__name__)    #Cria uma instância da classe Flask. O parâmetro __name__
-#é usado para identificar o módulo atual e ajudar o Flask a localizar recursos como templates e arquivos estáticos.
+@app.route('/')  # Define a rota para a URL raiz ('/')
+def home():  # Função executada quando '/' é acessado
+
+    # Variável da página HTML
+    toPage = {
+        # Valor da tag <title> → Título da página
+        'title': 'FlaskBlog',
+        # Nome da folha de estilos desta página (opcional)
+        # 'css': 'home.css',
+        # Nome do JavaScript desta página (opcional)
+        'js': 'home.js'
+        # Outros pares "chave" : "valor" entram aqui
+    }
+
+    # Abre a página de template → layout.html
+    # Passa a variável local `toPage` para o template como `page`
+    return render_template('home.html', page=toPage)
 
 
-@app.route('/') #Define uma rota para a URL raiz ('/'). 
-#O decorador @app.route é usado para associar uma função a um URL específico. Quando um usuário acessa a URL raiz, a função home será chamada.
-#python
+@app.route('/contacts')  # Define a rota para a URL '/contatos'
+def contacts():  # Função executada quando '/contacts' é acessado
+
+    # Variável da página HTML
+    toPage = {
+        'title': 'Faça contato',
+        'css': 'contacts.css',
+        'js': 'contacts.js'
+    }
+
+    # Retorna uma mensagem simples
+    return render_template('contacts.html', page=toPage)
 
 
-def home():
- #Define a função home, que será executada quando alguém acessar a URL associada à rota '/'.
-
-
-
-    return "Hello World!"
-
-@app.route('/contatos')
-def contacts():
-    return "Diga o que você quer!"
-
+# Verifica se o script está sendo executado diretamente
 if __name__ == '__main__':
+    # Inicia o servidor Flask em modo debug
     app.run(debug=True)
